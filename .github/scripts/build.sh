a04960f74f362d4d821b6a1a8074dffac5d3c0f9
Chen Fang <mtdcy.chen@gmail.com>
2025-04-20 13:13:02 +0800

    workflows code refactor

---
+++ .github/scripts/build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+pwd -P
+set -ex
+
+info() {
+    echo -e "🐳\\033[34m [$(date '+%Y/%m/%d %H:%M:%S')] $* \\033[0m" >&2
+}
+          
+export CL_LOGGING=silent
+export CL_MIRRORS="$(cat cl_mirrors)"
+export CL_CCACHE=0
+export CL_NJOBS=1
+
+ret=0
+
+IFS=', ' read -r -a cmdlets < .cmdlets
+for x in ${cmdlets[@]}; do
+    info "*** build $x ***"
+    bash ulib.sh build "$x" || ret=$?
+done
+
+IFS='@:' read -r user host port dest < cl_artifacts
+if test -n "$dest"; then
+    remote="$user@$host:$dest"
+    ssh_opt=(-p "$port" -o StrictHostKeyChecking=no)
+    if [ -f cl_ssh_token ]; then
+        chmod 0600 cl_ssh_token
+        ssh_opt+=(-i cl_ssh_token)
+    fi
+
+    info "*** rsync artifacts to remote ***"
+    rsync -avc -e "ssh ${ssh_opt[*]}" prebuilts/ "$remote/cmdlets/latest/" || ret=$?
+
+    info "*** rsync logs to remote ***"
+    rsync -avc -e "ssh ${ssh_opt[*]}" logs/ "$remote/cmdlets/logs/" || ret=$?
+
+    info "*** rsync packages to remote ***"
+    rsync -avc -e "ssh ${ssh_opt[*]}" packages/ "$remote/packages/" || ret=$?
+fi
+
+if [ "$ret" -ne 0 ]; then
+    text="Build $(basename "$(git rev-parse --show-toplevel)") failed
+    ---
+    $(git show HEAD --stat)
+    " 
+
+    curl --fail -sL --form-string "text=$text" "$(cat cl_notify)"
+fi
+
+exit "$ret"
---
+++ .github/scripts/prepare.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# prepare.sh [cmdlet1,cmdlet2,...]
+
+pwd -P
+set -ex
+# chown -R buildbot:buildbot .
+
+# fix: detected dubious ownership in repository
+git config --global --add safe.directory "$PWD"
+          
+if which brew; then
+    brewprefix="$(brew --prefix)"
+    export PATH="$brewprefix/opt/coreutils/libexec/gnubin:$PATH"
+    export PATH="$brewprefix/opt/gnu-sed/libexec/gnubin:$PATH"
+    export PATH="$brewprefix/opt/grep/libexec/gnubin:$PATH"
+    export PATH="$brewprefix/opt/gnu-tar/libexec/gnubin:$PATH"
+    export PATH="$brewprefix/opt/findutils/libexec/gnubin:$PATH"
+    make prepare-host
+elif test -n "$MSYSTEM"; then
+    chown -R buildbot:buildbot .
+fi
+echo "$PATH"
+
+if test -n "$1"; then
+    IFS=', ' read -r -a cmdlets <<< "$@"
+else
+    files=($(git show --pretty="" --name-only HEAD | grep -w "^libs")) || true
+    for x in "${files[@]}"; do
+        x=$(basename ${x%.u})
+        [[ "$x" =~ ^\. ]] && continue  ## ignored files
+        [[ "$x" =~ ^@  ]] && continue  ## ignored files
+        [[ "$x" =~ ^_  ]] && continue  ## ignored files
+        cmdlets+=("$x")
+    done
+    [ -n "${cmdlets[*]}" ] || cmdlets=(zlib)
+
+    # always expand ALL
+    [ "${cmdlets[*]}" = ALL ] && cmdlets=($(bash ulib.sh _deps_get ALL)) || true
+fi
+
+echo "${cmdlets[*]}" > .cmdlets
