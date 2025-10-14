64ff09cd1a64d23a651f6f85afbf175f3d388a52
Chen Fang <mtdcy@MacMini-M4.local>
2025-10-14 20:02:23 +0800

    add cjson.s

---
+++ libs/cjson.s
@@ -0,0 +1,23 @@
+#  Ultralightweight JSON parser in ANSI C
+
+# shellcheck disable=SC2034
+libs_lic="MIT"
+libs_ver=1.7.19
+libs_url=https://github.com/DaveGamble/cJSON/archive/refs/tags/v$libs_ver.tar.gz
+libs_sha=7fa616e3046edfa7a28a32d5f9eacfd23f92900fe1f8ccd988c1662f30454562
+
+libs_args=(
+    -DENABLE_CJSON_UTILS=ON
+    -DENABLE_CJSON_TEST=OFF
+
+    -DBUILD_SHARED_LIBS=OFF
+    -DCJSON_BUILD_SHARED_LIBS=OFF
+)
+
+libs_build() {
+    mkdir -p build && cd build && cmake .. || return 1
+
+    make
+}
+
+# vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
