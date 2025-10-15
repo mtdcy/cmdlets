baa8b321860ac9d8f0630f300c308d027f8cf591
Chen Fang <mtdcy@MacMini-M4.local>
2025-10-15 09:08:29 +0800

    enable raw image files support

--- libs/imagemagick.s
+++ libs/imagemagick.s
@@ -4,15 +4,13 @@
 libs_ver=7.1.2-5
 libs_url=https://imagemagick.org/archive/releases/ImageMagick-$libs_ver.tar.xz
 libs_sha=3f8a2ef3744a704edec90734106107a6f4548e65a30d91d4dedce4c17c6f9e75
-libs_dep=( freetype turbojpeg png tiff openjpeg webp xz bzip2 libxml2 zlib )
+libs_dep=( freetype libraw turbojpeg png tiff openjpeg webp xz bzip2 libxml2 zlib )
 
 # configure args
 libs_args=(
     --disable-option-checking
     --enable-silent-rules
     --disable-dependency-tracking
-
-    --enable-osx-universal-binary=no
 
     --disable-opencl
 
@@ -37,6 +35,7 @@ libs_args=(
     --without-pango
     --without-wmf
     --without-lcms
+    --without-x
 
     # openmp not ready
     --disable-openmp
@@ -46,15 +45,16 @@ libs_args=(
     --enable-static
 )
 
-#if is_darwin; then
-#    libs_args+=(
-#        --without-x
-#        # Work around checking for clang option to support OpenMP... unsupported
-#        ac_cv_prog_c_openmp="'-Xpreprocessor -fopenmp'"
-#        ac_cv_prog_cxx_openmp="'-Xpreprocessor -fopenmp'"
-#    )
-#    export LDFLAGS+=" -lomp -lz"
-#fi
+if is_darwin; then
+    libs_args+=(
+        --enable-osx-universal-binary=no
+
+        ## Work around checking for clang option to support OpenMP... unsupported
+        #ac_cv_prog_c_openmp="'-Xpreprocessor -fopenmp'"
+        #ac_cv_prog_cxx_openmp="'-Xpreprocessor -fopenmp'"
+    )
+    #export LDFLAGS+=" -lomp -lz"
+fi
 
 libs_build() {
     configure && make || return 1
