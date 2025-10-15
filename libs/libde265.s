26f01776a8d421e600d15701d0ab9438a30201cd
Chen Fang <mtdcy@MacMini-M4.local>
2025-10-15 14:17:41 +0800

    Revert "fix libde265 by disable dec265 binary"
    This reverts commit cc9b8c510a21d304dfc1129b6c32ad98a9a54487.

--- libs/libde265.s
+++ libs/libde265.s
@@ -18,7 +18,7 @@ libs_args=(
     --disable-dependency-tracking
 
     --disable-sherlock265
-    --disable-dec265         # needs sdl2
+    --enable-dec265         # needs sdl2
    
     # static
     --disable-shared
@@ -29,11 +29,6 @@ libs_build() {
     if is_darwin && is_arm64; then
         libs_args+=( --build=aarch64-apple-darwin )
     fi
-    
-    is_darwin || export CXXFLAGS+=" -static-libstdc++"
-
-    # no tools
-    sed -i '/SUBDIRS+=tools/d' Makefile.am
 
     configure && make || return 1
 
@@ -42,13 +37,12 @@ libs_build() {
     pkgfile libde265                  \
             include/libde265          \
             lib/libde265.a            \
-            lib/pkgconfig/libde265.pc
+            lib/pkgconfig/libde265.pc \
+            &&
 
-    # FIXME: can not find where is wrong
-    #  attempted static link of dynamic object `.../libstdc++.so'
-    #cmdlet  ./dec265/dec265 &&
+    cmdlet  ./dec265/dec265 &&
 
-    #check dec265 --version
+    check dec265 --version
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
