b61b346612f1bf1d0edec517e96d5a6a5b06fe79
Chen Fang <mtdcy@MacMini-M4.local>
2025-10-17 07:21:49 +0800

    libogg: fix urls

--- libs/libogg.s
+++ libs/libogg.s
@@ -5,8 +5,8 @@
 libs_lic="BSD"
 libs_ver=1.3.6
 libs_url=(
-    https://github.com/xiph/libogg/releases/download/v$libs_ver/libogg-$libs_ver.tar.gz
-    https://downloads.xiph.org/releases/libogg/libogg-$libs_ver.tar.gz
+    https://github.com/xiph/ogg/releases/download/v$libs_ver/libogg-$libs_ver.tar.gz
+    https://downloads.xiph.org/releases/ogg/libogg-$libs_ver.tar.gz
 )
 libs_sha=83e6704730683d004d20e21b8f7f55dcb3383cdf84c0daedf30bde175f774638
 
