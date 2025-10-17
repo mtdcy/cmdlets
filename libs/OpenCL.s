8a343df1cab50cc8446177b0b2a3cd7d1e91f7fc
Chen Fang <mtdcy@MacMini-M4.local>
2025-10-17 14:49:42 +0800

    OpenCL: use pkgfile() with make install

--- libs/OpenCL.s
+++ libs/OpenCL.s
@@ -1,4 +1,4 @@
-
+# OpenCL Headers
 #
 # shellcheck disable=SC2034
 libs_lic="GPL"
@@ -7,7 +7,9 @@ libs_url=https://github.com/KhronosGroup/OpenCL-Headers/archive/refs/tags/v$libs
 libs_sha=cc55c351b1b71346469c7ecdec3d62ad6661b2e3c41b9df165ee8b1ab75563d3
 
 libs_build() {
-    cmake . && make && make install
+    cmake . && make &&
+
+    pkgfile libOpenCL -- make install
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
