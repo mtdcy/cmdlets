d186681f8a85e46bf3447a702354664bad770205
Chen Fang <mtdcy@MacMini-M4.local>
2025-10-17 08:47:48 +0800

    libiconv: use pkgfile() with make install

--- libs/libiconv.s
+++ libs/libiconv.s
@@ -9,46 +9,42 @@ libs_ver=1.18
 libs_url=https://ftpmirror.gnu.org/gnu/libiconv/libiconv-$libs_ver.tar.gz
 libs_sha=3b08f5f4f9b4eb82f151a7040bfd6fe6c6fb922efe4b1659c66ea933276965e8
 
+# fails
+#is_darwin && libs_patches=(
+#    https://raw.githubusercontent.com/Homebrew/patches/9be2793af/libiconv/patch-utf8mac.diff
+#)
+
 libs_args=(
     --disable-option-checking
     --disable-dependency-tracking
     --enable-silent-rules
 
+    --enable-pic
     --enable-extra-encodings
 
     # no these for single static executables
     --disable-nls
     --disable-rpath
 
+    # static only
     --disable-shared
     --enable-static
 )
 
+#  Linux glibc/musl provides iconv.h 
 libs_build() {
-    export CFLAGS+=" -Wno-error=implicit-function-declaration"
+    # Reported at https://savannah.gnu.org/bugs/index.php?66170
+    is_darwin && export CFLAGS+=" -Wno-incompatible-function-pointer-types"
 
-    configure &&
 
-    # deparallels
-    make V=1 -j1 &&
+    configure && make && make check || return $?
 
-    # check & install
-    #make check &&
-
-    #make install &&
-    library libiconv \
-            include     include/iconv.h \
-            lib         lib/.libs/libiconv.{a,la} \
-            &&
-    library libcharset \
-            include     libcharset/include/{libcharset.h,localcharset.h} \
-            lib         libcharset/lib/.libs/libcharset.{a,la} \
-            &&
+    pkgfile libiconv -- make install &&
 
     cmdlet  src/iconv_no_i18n iconv &&
 
     # visual check
-    check iconv
+    check iconv --version
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
