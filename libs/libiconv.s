e422662bdffa63463d235d4cecaa22c6467b9045
Chen Fang <mtdcy@MacMini-M4.local>
2025-10-18 08:17:47 +0800

    libiconv: add inline patch for utf8mac

--- libs/libiconv.s
+++ libs/libiconv.s
@@ -9,10 +9,9 @@ libs_ver=1.18
 libs_url=https://ftpmirror.gnu.org/gnu/libiconv/libiconv-$libs_ver.tar.gz
 libs_sha=3b08f5f4f9b4eb82f151a7040bfd6fe6c6fb922efe4b1659c66ea933276965e8
 
-# fails
-#is_darwin && libs_patches=(
-#    https://raw.githubusercontent.com/Homebrew/patches/9be2793af/libiconv/patch-utf8mac.diff
-#)
+is_darwin && libs_patches=(
+    https://raw.githubusercontent.com/Homebrew/patches/9be2793af/libiconv/patch-utf8mac.diff
+)
 
 libs_args=(
     --disable-option-checking
@@ -35,7 +34,6 @@ libs_build() {
     # Reported at https://savannah.gnu.org/bugs/index.php?66170
     is_darwin && export CFLAGS+=" -Wno-incompatible-function-pointer-types"
 
-
     configure && make && make check || return $?
 
     pkgfile libiconv -- make install &&
@@ -47,3 +45,17 @@ libs_build() {
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
+
+__END__
+diff --git a/lib/flags.h b/lib/flags.h
+index d7cda21..4cabcac 100644
+--- a/lib/flags.h
++++ b/lib/flags.h
+@@ -14,6 +14,7 @@
+
+ #define ei_ascii_oflags (0)
+ #define ei_utf8_oflags (HAVE_ACCENTS | HAVE_QUOTATION_MARKS | HAVE_HANGUL_JAMO)
++#define ei_utf8mac_oflags (HAVE_ACCENTS | HAVE_QUOTATION_MARKS | HAVE_HANGUL_JAMO)
+ #define ei_ucs2_oflags (HAVE_ACCENTS | HAVE_QUOTATION_MARKS | HAVE_HANGUL_JAMO)
+ #define ei_ucs2be_oflags (HAVE_ACCENTS | HAVE_QUOTATION_MARKS | HAVE_HANGUL_JAMO)
+ #define ei_ucs2le_oflags (HAVE_ACCENTS | HAVE_QUOTATION_MARKS | HAVE_HANGUL_JAMO)
