dcc5348a42f99ea02c22a9592a872aaeb08f64b6
Chen Fang <mtdcy@MacMini-M4.local>
2025-10-19 13:32:57 +0800

    exiv2: add libssh support

--- libs/exiv2.s
+++ libs/exiv2.s
@@ -6,7 +6,7 @@ libs_lic="GPL-2.0-or-later"
 libs_ver=0.28.7
 libs_url=https://github.com/Exiv2/exiv2/archive/refs/tags/v$libs_ver.tar.gz
 libs_sha=5e292b02614dbc0cee40fe1116db2f42f63ef6b2ba430c77b614e17b8d61a638
-libs_dep=( zlib expat brotli inih libiconv curl )
+libs_dep=( zlib expat brotli inih libssh libiconv curl )
 
 # configure args
 libs_args=(
@@ -19,7 +19,7 @@ libs_args=(
     -Dxmp=enabled
 
     -Dvideo=true
-   
+
     # web ready
     -Dwebready=true
     -Dcurl=enabled
@@ -30,14 +30,14 @@ libs_args=(
 
 libs_build() {
     mkdir -p build
-    
-    meson setup build && 
+
+    meson setup build &&
 
     meson compile -C build --verbose || return 1
 
     pkgfile libexiv2 -- meson install -C build --tags devel
 
-    cmdlet ./build/exiv2 && 
+    cmdlet ./build/exiv2 &&
 
     check exiv2 --version
 }
