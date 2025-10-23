875b8a429ce8b90087de9c3b7aee133cc24a6b5d
Chen Fang <mtdcy@MacMini-M4.local>
2025-10-23 15:39:23 +0800

    update coreutils

--- libs/coreutils.s
+++ libs/coreutils.s
@@ -9,16 +9,17 @@ libs_sha=4a847a3aaf241d11f07fdc04ef36d73c722759675858665bc17e94f56c4fbfb3
 libs_dep=()
 
 utils=(
-    arch base32 base64 
-    cat echo cp rm ln install
+    arch base32 base64
+    cp rm ls ln install
     readlink unlink realpath
-    uniq numfmt 
+    sort uniq wc
+    cat echo printf numfmt
 )
 
 libs_args=(
     --release
     --verbose
-    
+
     --no-default-features
     --features "'${utils[*]}'"
 )
