d1b18a3a27cbece3b1c7f1f2edccd5acc1d09490
Chen Fang <mtdcy@MacMini-M4.local>
2025-10-23 19:05:19 +0800

    update gsed config

--- libs/gsed.s
+++ libs/gsed.s
@@ -4,7 +4,10 @@
 
 libs_lic='GPL-3.0-or-later'
 libs_ver=4.9
-libs_url=https://ftpmirror.gnu.org/gnu/sed/sed-$libs_ver.tar.xz
+libs_url=(
+    https://mirrors.ustc.edu.cn/gnu/sed/sed-$libs_ver.tar.xz
+    https://ftpmirror.gnu.org/gnu/sed/sed-$libs_ver.tar.xz
+)
 libs_sha=6e226b732e1cd739464ad6862bd1a1aba42d7982922da7a53519631d24975181
 libs_dep=(libiconv)
 
@@ -21,20 +24,23 @@ libs_args=(
     --disable-nls
 
     #--without-libiconv-prefix
-    #--without-libintl-prefix
+
+    # no nls or i18n
+    --disable-nls
+    --disable-i18n
+    --without-libintl-prefix
 
     --disable-doc
     --disable-man
-
-    # install as 'gsed'
-    --program-prefix=g
 )
 
 libs_build() {
-    configure && make &&
+    configure
+
+    make
 
     # install as gsed and symlink to sed
-    cmdlet sed/sed gsed sed &&
+    cmdlet ./sed/sed gsed sed
 
     check gsed --version
 }
