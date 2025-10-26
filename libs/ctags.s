3844998cd466ab1972f1cfae78f078c1bf2deb2a
Chen Fang <mtdcy@MacMini-M4.local>
2025-10-26 16:18:03 +0800

    Revert "[AutoUpdated] update libs"
    This reverts commit dfee0326d1375ae07a1a66f871f00fdf7aa3de5a.

--- libs/ctags.s
+++ libs/ctags.s
@@ -3,9 +3,9 @@
 # shellcheck disable=SC2034
 libs_name=ctags
 libs_lic="GPL-2.0-only"
-libs_ver=6.2.1
+libs_ver=6.2.0
 libs_url=https://github.com/universal-ctags/ctags/releases/download/v$libs_ver/universal-ctags-$libs_ver.tar.gz
-libs_sha=2c63efe9e0e083dc50e6fdd8c5414781cc8873d8c8940cf553c01870ed962f8c
+libs_sha=ae550fb8c5fdb5dfca2b1fc51a5de69300eddca9eb04bda9cc47b9703041763e
 libs_dep=(pcre2 libyaml jansson libxml2 libiconv)
 
 libs_args=(
