9c25d11ff375d5c4a37d7d66dbc086ec79d2d0ae
Chen Fang <mtdcy@MacMini-M4.local>
2025-10-29 17:03:44 +0800

    krb5: build static client libraries for Linux and macOS

--- libs/krb5.s
+++ libs/krb5.s
@@ -22,15 +22,34 @@ libs_args=(
 
 libs_build() {
     # macOS has krb5
-    depends_on is_linux
+    #depends_on is_linux
 
-    cd src
+    cd src || die
+
+    ## force static
+    #sed -i configure                             \
+    #    -e 's/CC_LINK_SHARED/CC_LINK_STATIC/g'   \
+    #    -e 's/CXX_LINK_SHARED/CXX_LINK_STATIC/g' \
 
     configure
 
-    make
+    # cc_initialize is only for _WIN32, but configure set it for macOS.
+    # refer winccld and stdcc in ccapi for details
+    #  => is this a mistake or bug?
+    sed -i '/USE_CCAPI_MACOS/d' include/autoconf.h
 
-    pkgfile libkrb5 -- make install
+    # build only static client libraries
+
+    # edit only top SUBDIRS
+    TOP_SUBDIRS=( util include lib build-tools )
+    sed -i 's/SUBDIRS/TOP_SUBDIRS/g' Makefile
+
+    make TOP_SUBDIRS="'${TOP_SUBDIRS[*]}'"
+
+    # no krb5-config
+    sed -i '/INSTALL.*krb5-config/d' build-tools/Makefile
+
+    pkgfile libkrb5 -- make install TOP_SUBDIRS="'${TOP_SUBDIRS[*]}'"
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
