ae80f2080b793e41072ecadc1272c3c3eab6e605
Chen Fang <mtdcy@MacMini-M4.local>
2025-10-31 10:13:09 +0800

    fixup! add dnsmasq => 2.91

--- libs/dnsmasq.s
+++ libs/dnsmasq.s
@@ -5,7 +5,9 @@ libs_lic='GPL-2.0+'
 libs_ver=2.91
 libs_url=https://thekelleys.org.uk/dnsmasq/dnsmasq-2.91.tar.gz
 libs_sha=2d26a048df452b3cfa7ba05efbbcdb19b12fe7a0388761eb5d00938624bd76c8
-libs_dep=( libidn2 nftables )
+libs_dep=( libidn2 nettle )
+
+is_linux && libs_dep+=( nftables )
 
 libs_args=(
 )
@@ -14,7 +16,10 @@ libs_build() {
     deparallelize
 
     # options
-    #COPTS="-DHAVE_NFTSET"
+    COPTS="-DHAVE_LIBIDN2 -DHAVE_DNSSEC -DNO_I18N -DNO_DBUS -DNO_UBUS"
+    if is_linux; then
+        COPTS+=" -DHAVE_CONNTRACK"
+    fi
 
     # Fix compilation on newer macOS versions.
     export CFLAGS+=" -D__APPLE_USE_RFC_3542"
@@ -35,6 +40,18 @@ libs_build() {
     cmdlet ./src/dnsmasq
 
     check dnsmasq --version
+
+    if is_linux; then
+        COPTS+=" -DNO_IPSET -DHAVE_NFTSET"
+
+        make clean || true
+
+        make PREFIX="'$PREFIX'" COPTS="'$COPTS'"
+
+        cmdlet ./src/dnsmasq dnsmasq-nftset
+
+        check dnsmasq-nftset --version
+    fi
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
