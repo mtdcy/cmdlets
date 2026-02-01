d78d6e983ac7bae703e84be9d92e73a73bd8ac9e
Chen Fang <mtdcy.chen@gmail.com>
2026-02-01 16:45:34 +0800

    update imagemagick => 7.1.2-13

--- libs/imagemagick.s
+++ libs/imagemagick.s
@@ -1,10 +1,14 @@
 # Tools and libraries to manipulate images in many formats
 
 # shellcheck disable=SC2034
-libs_ver=7.1.2-7
+libs_ver=7.1.2-13
 libs_url=https://github.com/ImageMagick/ImageMagick/archive/refs/tags/$libs_ver.tar.gz
-libs_sha=d532c7be0b4fbd17d03ef311f55ad8a4845c63cb74f8725320b7d3d3c6a7a4f7
-libs_dep=( freetype libraw libjpeg-turbo openjpeg libpng libtiff libwebp libheif xz bzip2 libxml2 zlib lcms2 liblqr imath )
+libs_sha=3617bffe497690ffe5b731227d026db1150e138ddb129481a1e202442e558512
+libs_dep=(
+    glib freetype lcms2
+    libxml2 liblqr imath
+    zlib
+)
 
 # configure args
 libs_args=(
@@ -15,24 +19,20 @@ libs_args=(
     --disable-opencl
 
     --with-freetype=yes
-
-    --with-jpeg         # JPEG
-    --with-openjp2      # JPEG 2000
-    --with-heic         # HEIF
-    --with-png
-    --with-tiff
-    --with-webp
-    --with-raw
-    
     --with-lcms
     --with-lqr
 
-    --with-modules
-    --with-zip
-    
+    # zlib only: automagically process .gz files
+    --with-zlib
+    --without-bzlib
+    --without-zstd
+    --without-lzma
+    --without-zip
+
     #--with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts
 
     # disabled features
+    --without-modules
     --without-fontconfig
     --without-gvc
     --without-jxl       # jpeg-xl
@@ -45,21 +45,48 @@ libs_args=(
     --without-x
 
     --disable-docs
-
     --disable-shared
     --enable-static
 )
 
-is_darwin && libs_args+=(
-    --enable-osx-universal-binary=no
-)
+is_darwin && libs_args+=( --enable-osx-universal-binary=no )
+
+# supported formats
+libs_dep+=( libraw )            && libs_args+=( --with-raw )        # RAW
+libs_dep+=( libjpeg-turbo )     && libs_args+=( --with-jpeg )       # JPEG
+libs_dep+=( openjpeg )          && libs_args+=( --with-openjp2 )    # JPEG 2000
+libs_dep+=( libpng )            && libs_args+=( --with-png )        # PNG
+libs_dep+=( libtiff )           && libs_args+=( --with-tiff )       # TIFF
+libs_dep+=( libwebp )           && libs_args+=( --with-webp )       # WEBP
+libs_dep+=( libheif )           && libs_args+=( --with-heic )       # HEIC
+
+# openmp
+is_darwin || libs_args+=( --enable-openmp )
 
 libs_build() {
-    configure && make &&
+    configure
 
-    cmdlet ./utilities/magick &&
+    make
 
-    check magick --version
+    # install configlib and configshare to the same directory.
+    sed -i '/CONFIGURE_PATH/s/etc/share/' Makefile
+
+    cmdlet.pkgfile ImageMagick -- make install-configlibDATA install-configshareDATA
+
+    cmdlet.install ./utilities/magick
+
+    cmdlet.check magick --version
+
+    cmdlet.caveats << EOF
+static built ImageMagick @ $libs_ver
+
+Configuration and resource files:
+
+    cmdlets.sh install ImageMagick
+    cmdlets.sh link share/ImageMagick-${libs_ver%%.*} ~/.config/ImageMagick
+
+    OR you can set MAGICK_CONFIGURE_PATH to where the files are.
+EOF
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
