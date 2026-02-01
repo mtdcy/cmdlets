86048c22055e78d1a6d6a59f145ea63d4e34750f
Chen Fang <mtdcy.chen@gmail.com>
2026-02-01 18:39:30 +0800

    update exiv2 build

--- libs/exiv2.s
+++ libs/exiv2.s
@@ -2,44 +2,45 @@
 #  replacement for exiftool which is perl scripts
 
 # shellcheck disable=SC2034
-libs_lic="GPL-2.0-or-later"
+libs_lic="GPLv2.0+"
 libs_ver=0.28.7
 libs_url=https://github.com/Exiv2/exiv2/archive/refs/tags/v$libs_ver.tar.gz
 libs_sha=5e292b02614dbc0cee40fe1116db2f42f63ef6b2ba430c77b614e17b8d61a638
-libs_dep=( zlib expat brotli inih libssh libiconv curl )
+libs_dep=( zlib expat brotli inih libiconv )
 
 # configure args
 libs_args=(
-    -Dbrotli=enabled
     -Diconv=enabled
     -Dinih=enabled
 
+    -Dpng=enabled       # Build with PNG support (requires zlib)
+    -Dbrotli=enabled    # Google brotli
+    -Dxmp=enabled       # Build with BMFF support
+
+    # BMFF types such as AVIF, CR3, HEIF and HEIC
     -Dbmff=true
-    -Dpng=enabled
-    -Dxmp=enabled
 
-    -Dvideo=true
+    # Support of video files is limited. Currently exiv2 only has some rudimentary support to read metadata from quicktime, matroska and riff based video files
+    -Dvideo=false
 
-    # web ready
-    -Dwebready=true
-    -Dcurl=enabled
+    # no webready => keep it simple
+    -Dwebready=false
 
     -Dnls=disabled
     -DunitTests=disabled
 )
 
 libs_build() {
-    mkdir -p build
 
-    meson setup build &&
+    meson.setup
 
-    meson compile -C build --verbose || return 1
+    meson.compile
 
-    pkgfile libexiv2 -- meson install -C build --tags devel
+    pkgfile libexiv2 -- meson.install --tags devel
 
-    cmdlet ./build/exiv2 &&
+    cmdlet.install ./build/exiv2
 
-    check exiv2 --version
+    cmdlet.check exiv2 --version --verbose
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
