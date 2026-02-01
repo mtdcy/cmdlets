a6d1f890a1b993cf92068149c6d06934001300a5
Chen Fang <mtdcy.chen@gmail.com>
2026-02-01 13:19:45 +0800

    fix libedit version number

--- libs/libedit.s
+++ libs/libedit.s
@@ -2,7 +2,7 @@
 
 # shellcheck disable=SC2034
 libs_lic='BSD-3-Clause'
-libs_ver=20251016-3.1
+libs_ver=3.1
 libs_url=https://thrysoee.dk/editline/libedit-20251016-3.1.tar.gz
 libs_sha=21362b00653bbfc1c71f71a7578da66b5b5203559d43134d2dd7719e313ce041
 libs_dep=( ncurses )
