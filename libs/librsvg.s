bf186bebdb0512958e9a45c5bbc72d7eabba2944
Chen Fang <mtdcy.chen@gmail.com>
2026-02-02 20:06:10 +0800

    add librsvg => 2.61.3

---
+++ libs/librsvg.s
@@ -0,0 +1,29 @@
+# Library to render SVG files using Cairo
+
+# shellcheck disable=SC2034
+libs_lic="LGPLv2.1+"
+libs_ver=2.61.3
+libs_url=https://download.gnome.org/sources/librsvg/2.61/librsvg-2.61.3.tar.xz
+libs_sha=a56d2c80d744ad2f2718f85df466fe71d24ff1f9bc3e5ef588bde4d7e87815f2
+libs_dep=( glib gdk-pixbuf cairo harfbuzz freetype fontconfig pango libpng libxml2 )
+
+# configure args
+libs_args=(
+    -Dpixbuf=enabled            # GDK-Pixbuf, depends on glib and gdk-pixbuf
+    -Dpixbuf-loader=disabled    # needs gdk-pixbuf-query-loaders
+
+    -Ddocs=disabled
+    -Dtests=False
+)
+
+libs_build() {
+    cargo.depends cargo-c
+
+    meson.setup
+
+    meson.compile
+
+    pkgfile librsvg -- meson.install --tags devel
+}
+
+# vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
