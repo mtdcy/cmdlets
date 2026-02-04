437f3e8b12c713a55e4e9f457ed57426767c0cfe
Chen Fang <mtdcy.chen@gmail.com>
2026-02-04 10:19:37 +0800

    update nghttp2 => 1.68.0

--- libs/nghttp2.s
+++ libs/nghttp2.s
@@ -2,9 +2,9 @@
 # shellcheck disable=SC2034
 libs_desc="HTTP/2 C Library"
 libs_lic="MIT"
-libs_ver=1.65.0
-libs_url=https://github.com/nghttp2/nghttp2/releases/download/v$libs_ver/nghttp2-$libs_ver.tar.gz
-libs_sha=8ca4f2a77ba7aac20aca3e3517a2c96cfcf7c6b064ab7d4a0809e7e4e9eb9914
+libs_ver=1.68.0
+libs_url=https://github.com/nghttp2/nghttp2/releases/download/v1.68.0/nghttp2-1.68.0.tar.gz
+libs_sha=2c16ffc588ad3f9e2613c3fad72db48ecb5ce15bc362fcc85b342e48daf51013
 libs_dep=()
 
 libs_args=(
@@ -22,9 +22,11 @@ libs_args=(
 )
 
 libs_build() {
-    configure  && make -C lib V=1 || return $?
+    configure
 
-    pkgfile libnghttp2 -- make install
+    make -C lib V=1
+
+    pkgfile libnghttp2 -- make.install
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
