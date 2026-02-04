e1c9b34edb15d616708c69e3d29ef7e9c6965c2d
Chen Fang <mtdcy.chen@gmail.com>
2026-02-05 07:27:17 +0800

    fix giflib build

--- libs/giflib.s
+++ libs/giflib.s
@@ -7,31 +7,27 @@ libs_url=https://downloads.sourceforge.net/project/giflib/giflib-$libs_ver.tar.g
 libs_sha=be7ffbd057cadebe2aa144542fd90c6838c6a083b5e8a9048b8ee3b66b29d5fb
 libs_dep=()
 
+libs_args=(
+)
+
 libs_build() {
-    make CFLAGS="'$CFLAGS'" LDFLAG="'$LDFLAGS'" gifecho &&
+    # no doc
+    sed '/-C doc/d' -i Makefile
 
-    cat > gif.pc << EOF
-prefix=$PREFIX
-exec_prefix=\${prefix}
-libdir=\${exec_prefix}/lib
-includedir=\${prefix}/include
+    make CFLAGS="'$CFLAGS'" LDFLAG="'$LDFLAGS'"
 
-Name: gif
-Version: $libs_ver
-
-Libs: -L\${libdir} -lgif
-Cflags: -I\${includedir}
-EOF
+    pkgconf gif.pc -lgif
 
     pkginst libgif                    \
             include         gif_lib.h \
             lib             libgif.a  \
-            lib/pkgconfig   gif.pc    \
-            &&
+            lib/pkgconfig   gif.pc
 
-    cmdlet gifecho &&
-
-    check gifecho -h
+    local ret=0 x
+    for x in gif2rgb gifbuild giffix giftext giftool gifclrmp; do
+        cmdlet.install "$x"
+        cmdlet.check "$x"
+    done
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
