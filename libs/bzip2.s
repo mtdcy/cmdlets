7e654a00abbb5266b91314140cb1ec70d5b7050c
Chen Fang <mtdcy.chen@gmail.com>
2026-02-08 17:32:14 +0800

    bzip2: fix mingw build

--- libs/bzip2.s
+++ libs/bzip2.s
@@ -11,22 +11,31 @@ libs_sha=ab5a03176ee106d3f0fa90e381da478ddae405918153cca248e682cd0c4a2269
 libs_build() {
     hack.makefile Makefile CC AR RANLIB CFLAGS LDFLAGS
 
-    make all test &&
+    make bzip2
+
+    # add file extension for windows
+    is_mingw && mv bzip2 bzip2.exe
+
+    # will not pass with mingw
+    is_mingw || make test
 
     pkgconf bz2     -lbz2
     pkgconf bzip2   -lbz2
     pkgconf libbz2  -lbz2
 
     # install lib and headers
-    pkginst libbz2 bzlib.h libbz2.a bz2.pc bzip2.pc libbz2.pc
+    cmdlet.pkginst libbz2 bzlib.h libbz2.a bz2.pc bzip2.pc libbz2.pc
 
     # install cmdlets and symlinks
-    cmdlet bzip2 bzip2 bunzip2 bzcat &&
-    cmdlet bzdiff bzdiff bzcmp &&
-    cmdlet bzgrep bzgrep bzegrep bzfgrep &&
-    cmdlet bzmore bzmore bzless &&
+    cmdlet.install bzip2 bzip2 bunzip2 bzcat
+    cmdlet.check bzip2 --version
 
-    check bzip2
+    # no shell scripts for windows
+    if ! is_mingw; then
+        cmdlet.install bzdiff bzdiff bzcmp
+        cmdlet.install bzgrep bzgrep bzegrep bzfgrep
+        cmdlet.install bzmore bzmore bzless
+    fi
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
