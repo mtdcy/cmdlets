a996ff7811654d181bcf2ecfae8530efd72c8f0e
Chen Fang <mtdcy.chen@gmail.com>
2026-02-08 23:08:59 +0800

    ncurses: fix mingw build

--- libs/ncurses.s
+++ libs/ncurses.s
@@ -10,43 +10,39 @@ libs_url=https://ftpmirror.gnu.org/gnu/ncurses/ncurses-$libs_ver.tar.gz
 libs_sha=355b4cbbed880b0381a04c46617b7656e362585d52e9cf84a67e2009b749ff11
 libs_dep=()
 
+# build a simple and fast ncurses library
 libs_args=(
     --disable-option-checking
     --enable-silent-rules
     --disable-dependency-tracking
 
-    --enable-pc-files
-    --with-pkg-config-libdir="$PKG_CONFIG_PATH"
+    --disable-overwrite         # put headers in subdir, omit link to -lcurses
+
+    # --datadir                 # terminfo location
+
+    # terminfo(ncurses) vs termcap(obsolete)
+    #  => Modern systems predominantly use terminfo
+    # https://invisible-island.net/ncurses/INSTALL.html#CONFIGURING-FALLBACK-ENTRIES
+    # pure-terminfo mode, no termcap => makes the ncurses library smaller and faster
+    --disable-termcap           # enable termcap for fallbacks => needs infocmp
+    --without-fallbacks         # no fallback to termcap
+    # The system's tic program is used to install the terminal database, even for cross-compiles.
+    --without-database          # terminfo database
 
     # libncurses with widec support
     --enable-widec
     --disable-lib-suffixes
 
-    --disable-nls
+    # misc
+    --enable-term-driver
+    --enable-sp-funcs
+    --enable-pc-files
+    --with-pkg-config-libdir="$PKG_CONFIG_PATH"
 
-    # from homebrew:ncurses.rb
-    --enable-sigwinch
-    --enable-symlinks
+    # disabled features
+    --disable-nls
     --with-gpm=no
     --without-ada
-
-    # from macports:ncurses
-    --enable-overwrite  # overwrite curses
-
-    # we are building static executable, cann't ship hardcoded prebuilts path into executables.
-    #
-    # terminfo search dirs
-    #  check with `infocmp -D'
-    #  override with env TERMINFO
-    #
-    # from debian:/etc/terminfo/README
-    --with-terminfo-dirs="/etc/terminfo:/lib/terminfo:/usr/share/terminfo"
-    # /usr/share/terminfo is a common path for both Linux and macOS
-    --with-default-terminfo-dir="/usr/share/terminfo"
-
-    # --with-fallbacks
-    --without-database
-    --enable-termcap # for fallbacks support
 
     # static without debug
     --without-shared
@@ -55,20 +51,37 @@ libs_args=(
     --without-manpages
 )
 
-libs_build() {
-    # install tic & infocmp first for fallbacks support
-    #  https://stackoverflow.com/questions/76290814/compile-ncurses-disable-database-why-nc-fallback-undefined
-    #  https://invisible-island.net/ncurses/INSTALL.html#CONFIGURING-FALLBACK-ENTRIES
-    configure && make PROGS="'tic infocmp'"
+if is_mingw; then
+    libs_args+=(
+        --disable-home-terminfo         # drop ~/.terminfo from terminfo search-path
+    )
+ else
+    # *nix system: avoid hardcoded PREFIX
+    libs_args+=(
+        # we are building static executable, cann't ship hardcoded prebuilts path into executables.
+        #
+        # terminfo search dirs, override with env TERMINFO
+        #
+        # from debian:/etc/terminfo/README
+        --with-terminfo-dirs="/etc/terminfo:/lib/terminfo:/usr/share/terminfo"
+        # /usr/share/terminfo is a common path for both Linux and macOS
+        --with-default-terminfo-dir="/usr/share/terminfo"
+    )
+fi
 
-    # prepare fallbacks
-    ./ncurses/tinfo/MKfallback.sh \
-        "$TERMINFO"               \
-        ./misc/terminfo.src       \
-        ./progs/tic               \
-        ./progs/infocmp           \
-        linux xterm               \
-        > ncurses/fallback.c
+libs_build() {
+    # mingw
+    if is_mingw; then
+        export BUILD_EXEEXT="$_BINEXT"
+        export PATH_SEPARATOR=";"
+
+        # EXT not working for these scripts
+        sed -i ncurses/tinfo/MKcaptab.sh \
+            -i ncurses/tinfo/MKuserdefs.sh \
+            -e "s/\<make_hash\>/make_hash$_BINEXT/g" || die
+    fi
+
+    configure
 
     make
 
@@ -81,7 +94,6 @@ libs_build() {
 
     #       source          target  links...
     cmdlet  ./progs/tic     tic     infotocap captoinfo
-    cmdlet  ./progs/infocmp
     cmdlet  ./progs/tset    tset    reset
     cmdlet  ./progs/clear
     cmdlet  ./progs/tabs
