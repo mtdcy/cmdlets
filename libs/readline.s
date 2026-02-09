b9dfb1df133d68adc1cd71a84a1e30d4014ea12c
Chen Fang <mtdcy.chen@gmail.com>
2026-02-09 11:00:59 +0800

    readline: fix mingw build

--- libs/readline.s
+++ libs/readline.s
@@ -1,21 +1,24 @@
 # Library for command-line editing
 #
 # BE CAREFUL: macOS provide libedit
+libs_stable=1
 
 # shellcheck disable=SC2034
-libs_lic='GPL-3.0-or-later'
+libs_lic='GPLv3.0+'
 libs_ver=8.3
 libs_url=(
     https://mirrors.ustc.edu.cn/gnu/readline/readline-$libs_ver.tar.gz
     https://ftpmirror.gnu.org/gnu/readline/readline-$libs_ver.tar.gz
 )
 libs_sha=fe5383204467828cd495ee8d1d3c037a7eba1389c22bc6a041f627976f9061cc
-libs_dep=(ncurses)
+libs_deps=( ncurses )
 
-# patch fails
-#libs_patches=(
-#    https://ftpmirror.gnu.org/gnu/readline/readline-8.3-patches/readline83-001
-#)
+libs_patches=(
+    https://ftp.gnu.org/gnu/readline/readline-8.3-patches/readline83-001
+    https://ftp.gnu.org/gnu/readline/readline-8.3-patches/readline83-002
+    https://ftp.gnu.org/gnu/readline/readline-8.3-patches/readline83-003
+)
+libs_patch_level=0
 
 libs_args=(
     --disable-option-checking
@@ -24,7 +27,6 @@ libs_args=(
 
     # use ncurses instead of termcap
     --with-curses
-    --enable-multibyte
 
     # no share/readline/*.c
     --disable-install-examples
@@ -35,7 +37,15 @@ libs_args=(
 )
 
 libs_build() {
-    configure && make && make check || return 1
+    # https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=mingw-w64-readline
+    # windows don't have struct winsize
+    is_mingw && sed -i '/^extern int _rl_tcgetwinsize/i struct winsize;' rlwinsize.h
+
+    configure
+
+    make
+
+    make check
 
     pkgfile libreadline -- make install-static
 }
