e90e27535c3b09c87c478c517ca75d63c943ab8c
Chen Fang <mtdcy.chen@gmail.com>
2026-02-10 10:40:52 +0800

    ldns: fix mingw build

--- libs/ldns.s
+++ libs/ldns.s
@@ -1,14 +1,28 @@
 # DNS library written in C
 
+libs_stable=1
+
 # shellcheck disable=SC2034
 libs_lic='BSD-3-Clause'
-libs_ver=1.9.0
+libs_ver=1.8.3
 libs_url=(
-    https://github.com/NLnetLabs/ldns/archive/refs/tags/1.9.0.tar.gz
-    https://nlnetlabs.nl/downloads/ldns/ldns-1.9.0.tar.gz
+    https://github.com/NLnetLabs/ldns/archive/refs/tags/1.8.3.tar.gz
+    https://nlnetlabs.nl/downloads/ldns/ldns-1.8.3.tar.gz
 )
-libs_sha=e882cdb6b30504623a799e724f77273c14d5f265c925a2884de9fbc94aa88d19
+libs_sha=33fb1a77f2de2fca9e749d17256334a3222a9e9d11b31c6d998bd920f3bd6776
 libs_dep=( openssl )
+
+# https://github.com/msys2/MINGW-packages/tree/master/mingw-w64-ldns
+if is_mingw; then
+    libs_patches=(
+        https://github.com/msys2/MINGW-packages/raw/refs/heads/master/mingw-w64-ldns/001-include-missing-header-ws2tcpip.patch
+        https://github.com/msys2/MINGW-packages/raw/refs/heads/master/mingw-w64-ldns/ldns-1.6.17-relocate.patch
+    )
+    libs_resources=(
+        https://github.com/msys2/MINGW-packages/raw/refs/heads/master/mingw-w64-ldns/pathtools.c
+        https://github.com/msys2/MINGW-packages/raw/refs/heads/master/mingw-w64-ldns/pathtools.h
+    )
+fi
 
 libs_args=(
     --with-ssl="'$PREFIX'" # DNSSEC
@@ -27,7 +41,10 @@ libs_args=(
 
 libs_build() {
     # fix libldns.pc
-    export LDFLAGS+=" $($PKG_CONFIG --libs openssl)"
+    #export LDFLAGS+=" $($PKG_CONFIG --libs openssl)"
+
+    # configure: ldns won't check libraries type and just do -lcrypto
+    export LIBS="$($PKG_CONFIG --libs-only-l libcrypto)"
 
     # sh: 0: cannot open ./install-sh: No such file, WHY?
     ln -sfv drill/install-sh ./
