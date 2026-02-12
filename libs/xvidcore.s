6ab42529d8bb31fbfa64f7a92d0ae9b08d6a5a88
Chen Fang <mtdcy.chen@gmail.com>
2026-02-12 13:55:57 +0800

    OpenCL: fix mingw build

--- libs/xvidcore.s
+++ libs/xvidcore.s
@@ -6,16 +6,33 @@ libs_ver=1.3.7
 libs_url=https://downloads.xvid.com/downloads/xvidcore-$libs_ver.tar.bz2
 libs_sha=aeeaae952d4db395249839a3bd03841d6844843f5a4f84c271ff88f7aa1acff7
 
+libs_patches=(
+    https://github.com/msys2/MINGW-packages/raw/refs/heads/master/mingw-w64-xvidcore/0001-remove-dll-option-clang.patch
+)
+
 libs_build() {
-    cd build/generic
+    pushd build/generic
 
-    configure --disable-shared --enable-static &&
+    if is_mingw; then
+        #1. x86_64-w64-mingw32-gcc-posix: error: unrecognized command-line option ‘-mno-cygwin’; did you mean ‘-mno-clwb’?
+        #2. fix STATIC_LIB 
+        sed -i configure \
+            -e 's/"-mno-cygwin"/""/g' \
+            -e 's/STATIC_LIB="xvidcore/STATIC_LIB="libxvidcore/g' \
+            || die "hack configure failed."
+    fi
 
-    make all
+    configure
 
-    pkginst xvidcore \
-        include ../../src/xvid.h \
-        lib =build/libxvidcore.a
+    make libxvidcore.a
+
+    # create pc file
+    pkgconf xvidcore.pc -lxvidcore
+
+    pkginst xvidcore                         \
+        include         ../../src/xvid.h     \
+        lib             =build/libxvidcore.a \
+        lib/pkgconfig   xvidcore.pc
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
