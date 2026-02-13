4c4b3b75e8dbc635b968788d89c22a90087cd92a
Chen Fang <mtdcy.chen@gmail.com>
2026-02-13 12:26:18 +0800

    lzip: fix mingw build

--- libs/lzip.s
+++ libs/lzip.s
@@ -4,28 +4,32 @@ libs_name=lzip
 libs_desc="LZMA lossless data compressor"
 
 libs_lic='GPL-2.0-or-later'
-libs_ver=1.24.1
+libs_ver=1.25
 libs_url=http://download.savannah.gnu.org/releases/lzip/lzip-$libs_ver.tar.gz
-libs_sha=30c9cb6a0605f479c496c376eb629a48b0a1696d167e3c1e090c5defa481b162
+libs_sha=09418a6d8fb83f5113f5bd856e09703df5d37bae0308c668d0f346e3d3f0a56f
 libs_dep=()
+
+is_mingw && CXXFLAGS+= '-D__USE_MINGW_ANSI_STDIO'
 
 # non-standard configure
 libs_args=(
-    PREFIX="'$PREFIX'"
-
     CXX="'$CXX'"
-    CXXFLAGS="'$CXXFLAGS'" 
     CPPFLAGS="'$CPPFLAGS'"
+    CXXFLAGS+="'$CXXFLAGS'" 
     LDFLAGS="'$LDFLAGS'"
 )
 
 libs_build() {
-    configure && make && make check || return $?
+    configure 
 
-    cmdlet lzip &&
+    make 
+
+    is_mingw || make check 
+
+    cmdlet.install lzip
 
     # verify
-    check lzip --version
+    cmdlet.check lzip --version
 }
 
 
