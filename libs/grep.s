7fa6bd7d1dc577fac9194ab419a13d4c25c5b62c
Chen Fang <mtdcy.chen@gmail.com>
2026-02-14 23:28:49 +0800

    grep: fix mingw build

--- libs/grep.s
+++ libs/grep.s
@@ -6,43 +6,40 @@ libs_lic='GPL-3.0-or-later'
 libs_ver=3.12
 libs_url=https://ftpmirror.gnu.org/gnu/grep/grep-$libs_ver.tar.xz
 libs_sha=2649b27c0e90e632eadcd757be06c6e9a4f48d941de51e7c0f83ff76408a07b9
-libs_dep=(libiconv pcre2)
+libs_deps=(libiconv pcre2)
 
 libs_args=(
     --disable-option-checking
     --enable-silent-rules
     --disable-dependency-tracking
 
-    --enable-perl-regexp # pcre2
-
-    --without-selinux
-    --disable-rpath
-
     # no i18n or nls
     --disable-nls
     --without-libintl-prefix
 
+    # disabled features
+    --without-selinux
     --disable-doc
     --disable-man
 )
 
-[[ ${libs_dep[*]} =~ libiconv ]] || libs_args+=(--without-libiconv-prefix)
+is_listed libiconv  "${libs_deps[@]}" && libs_args+=( --with-libiconv      ) || libs_args+=( --without-libiconv-prefix )
+is_listed pcre2     "${libs_deps[@]}" && libs_args+=( --enable-perl-regexp ) || libs_args+=( --disable-perl-regexp     )
 
 libs_build() {
     # note: egrep & fgrep are obsolescent;
     configure
 
-    make -C lib
-    make -C src grep
+    make
 
     # check: grep with pcre
-    echo FOO | ./src/grep -P '(?i)foo' || die "check grep with pcre failed"
+    echo FOO | run src/grep -P '(?i)foo' || die "check grep with pcre failed"
 
     # install grep
-    cmdlet ./src/grep
+    cmdlet.install src/grep
 
     # verify
-    check grep
+    cmdlet.check grep --version
 }
 
 
