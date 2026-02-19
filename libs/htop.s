6ae5c46c88fbb975f42a642694c76cf2b938e19b
Chen Fang <mtdcy.chen@gmail.com>
2026-02-19 19:12:01 +0800

    htop: no mingw build

--- libs/htop.s
+++ libs/htop.s
@@ -1,25 +1,38 @@
 # Improved top (interactive process viewer)
 
+libs_targets=( linux macos )
+
 # shellcheck disable=SC2034
 libs_name=htop
 libs_lic="GPL"
 libs_ver=3.4.1
 libs_url=https://github.com/htop-dev/htop/releases/download/$libs_ver/htop-$libs_ver.tar.xz
 libs_sha=904f7d4580fc11cffc7e0f06895a4789e0c1c054435752c151e812fead9f6220
-libs_dep=(ncurses) # enables mouse scroll
+
+libs_deps=(ncurses) # enables mouse scroll
+
+libs_args=(
+    --disable-option-checking
+    --enable-silent-rules
+    --enable-dependency-tracking
+
+    --enable-static
+)
 
 libs_build() {
-    ./autogen.sh &&
+    # wrong htop_git_version
+    sed -i configure.ac \
+        -e 's/\[git describe.*\]/[echo '']/'
 
-    configure &&
+    bootstrap
 
-    make &&
+    configure 
 
-    make install &&
+    make 
 
-    cmdlet htop &&
+    cmdlet.install htop
 
-    check htop
+    cmdlet.check htop --version
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
