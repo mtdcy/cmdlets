e9d200152683d3b327eaa5fb53384e2b8b8fed33
Chen Fang <mtdcy.chen@gmail.com>
2026-02-26 16:59:21 +0800

    libargp: no libintl

--- libs/libargp.s
+++ libs/libargp.s
@@ -5,8 +5,7 @@ libs_ver=1.5.0
 libs_url=https://github.com/argp-standalone/argp-standalone/archive/refs/tags/1.5.0.tar.gz
 libs_sha=c29eae929dfebd575c38174f2c8c315766092cec99a8f987569d0cad3c6d64f6
 
-# libintl from glib, otherwise intl from libc
-is_mingw && libs_deps=( glib )
+libs_deps=( )
 
 # configure args
 libs_args=(
@@ -14,13 +13,15 @@ libs_args=(
 )
 
 libs_build() {
+    sed -i '/HAVE_LIBINTL_H/d' meson.build
+
     meson.setup
 
     meson.compile
 
-    pkgconf "$PREFIX/lib/pkgconfig/argp.pc" -largp
+    cmdlet.pkgconf "$PREFIX/lib/pkgconfig/argp.pc" -largp
 
-    pkgfile "$libs_name" lib/pkgconfig/argp.pc -- meson.install
+    cmdlet.pkgfile "$libs_name" lib/pkgconfig/argp.pc -- meson.install
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
