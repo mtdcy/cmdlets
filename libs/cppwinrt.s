aea614f2d8b4b496ae4c44e425acbafaed28cda0
Chen Fang <mtdcy.chen@gmail.com>
2026-02-28 08:24:12 +0800

    cppwinrt: fix build on case insensitive system

--- libs/cppwinrt.s
+++ libs/cppwinrt.s
@@ -11,18 +11,22 @@ libs_sha=bf68d4f4bb1cb03bc398a05cd70cf92a92315d3e96e83c65aa1dc213d1f90189
 # configure args
 libs_build() {
     # Windows and mingw headers are case insensitive
+    #  => but our build system runs on Linux which is case sensitive
+    #   => rename all files to lower case
     while read -r x; do
+        x="${x#mingw64/}"
+        mkdir -p "${x%/*}"
+        cp "mingw64/$x" "$(echo "$x" | tr A-Z a-z)" || true
         sed -i '/include .*Windows\..*\.h/s/.*/\L&/' "$x"
-        mv "$x" "$(echo "$x" | tr A-Z a-z)" || true
     done < <(find mingw64/include -name "*.h")
 
-    # -std=c++20    : error: #error C++/WinRT requires coroutine support
+    # -std=c++20    : error: C++/WinRT requires coroutine support
     # -fpermissive  : error: invalid conversion
-    pkgconf libwinrt.pc "-I$PREFIX/include/winrt" -std=c++20 -fpermissive
+    pkgconf libwinrt.pc -std=c++20 -fpermissive
 
-    cmdlet.pkginst libwinrt \
-        include/winrt mingw64/include/winrt/*.h \
-        include/winrt/impl mingw64/include/winrt/impl/*.h \
+    cmdlet.pkginst libwinrt                        \
+        include/winrt       include/winrt/*.h      \
+        include/winrt/impl  include/winrt/impl/*.h \
         libwinrt.pc
 
     cmdlet.install mingw64/bin/cppwinrt.exe
