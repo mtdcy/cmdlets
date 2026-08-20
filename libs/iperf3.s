79c498a8701bcbb1c087ab29e249fb9203b19610
mtdcy <mtdcy.chen@gmail.com>
2026-08-20 20:41:16 +0800

    ♻️ fix iperf3 build

--- libs/iperf3.s
+++ libs/iperf3.s
@@ -6,30 +6,30 @@ libs_lic='BSD-3-Clause'
 libs_ver=3.18
 libs_url=https://github.com/esnet/iperf/releases/download/$libs_ver/iperf-$libs_ver.tar.gz
 libs_sha=c0618175514331e766522500e20c94bfb293b4424eb27d7207fb427b88d20bab
-libs_dep=()
+libs_dep=(openssl)
 
 libs_args=(
     --disable-option-checking
     --enable-silent-rules
     --disable-dependency-tracking
 
+    --with-openssl="'$PREFIX'"
+
     --disable-shared
     --enable-static
 )
 
 libs_build() {
-    configure &&
+    configure
 
-    make &&
+    make
 
     # check
     #make check &&
-
-    cmdlet ./src/iperf3 &&
+    cmdlet.install ./src/iperf3
 
     # verify
-    check iperf3
+    cmdlet.check iperf3
 }
-
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
