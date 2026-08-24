b7c7f6f4e029c3815c3ec1e84b73eeb94ab81569
mtdcy <mtdcy.chen@gmail.com>
2026-08-25 07:54:43 +0800

    ♻️ pixman: disable openmp except build for linux

--- libs/pixman.s
+++ libs/pixman.s
@@ -5,12 +5,15 @@ libs_lic="MIT"
 libs_ver=0.46.4
 libs_url=https://cairographics.org/releases/pixman-0.46.4.tar.gz
 libs_sha=d09c44ebc3bd5bee7021c79f922fe8fb2fb57f7320f55e97ff9914d2346a591c
-libs_dep=( )
+libs_dep=()
 
 # configure args
 libs_args=(
     -Ddemos=disabled
+    -Dtests=disabled
 )
+
+is_linux || libs_args+=(-Dopenmp=disabled)
 
 libs_build() {
 
@@ -18,7 +21,7 @@ libs_build() {
 
     meson.compile
 
-    pkgfile libpixman -- meson.install --tags devel
+    cmdlet.pkgfile libpixman -- meson.install --tags devel
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
