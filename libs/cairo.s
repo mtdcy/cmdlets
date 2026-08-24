a1a269c3ceaf55cbe6ad79c8b509f82df492f489
mtdcy <mtdcy.chen@gmail.com>
2026-08-25 06:45:08 +0800

    ♻️ fix cairo mingw build

--- libs/cairo.s
+++ libs/cairo.s
@@ -7,10 +7,10 @@ libs_rev=1
 libs_url=https://cairographics.org/releases/cairo-1.18.4.tar.xz
 libs_sha=445ed8208a6e4823de1226a74ca319d3600e83f6369f99b14265006599c32ccb
 
-libs_deps=( zlib lzo glib libpng freetype fontconfig pixman )
+libs_deps=(zlib lzo glib libpng freetype fontconfig pixman)
 
 # X Window System
-is_linux && libs_deps+=( libxcb )
+is_linux && libs_deps+=(libxcb)
 
 libs_args=(
     -Dxlib=disabled     # without X windows
@@ -19,27 +19,32 @@ libs_args=(
     -Dtests=disabled    # without tests
 )
 
-is_listed zlib       libs_deps && libs_args+=( -Dzlib=enabled       ) || libs_args+=( -Dzlib=disabled       )
-is_listed lzo        libs_deps && libs_args+=( -Dlzo=enabled        ) || libs_args+=( -Dlzo=disabled        )
-is_listed glib       libs_deps && libs_args+=( -Dglib=enabled       ) || libs_args+=( -Dglib=disabled       )
-is_listed libpng     libs_deps && libs_args+=( -Dpng=enabled        ) || libs_args+=( -Dpng=disabled        )
-is_listed libxcb     libs_deps && libs_args+=( -Dxcb=enabled        ) || libs_args+=( -Dxcb=disabled        )
-is_listed freetype   libs_deps && libs_args+=( -Dfreetype=enabled   ) || libs_args+=( -Dfreetype=disabled   )
-is_listed fontconfig libs_deps && libs_args+=( -Dfontconfig=enabled ) || libs_args+=( -Dfontconfig=disabled )
+is_listed zlib       libs_deps && libs_args+=(-Dzlib=enabled)         || libs_args+=(-Dzlib=disabled)
+is_listed lzo        libs_deps && libs_args+=(-Dlzo=enabled)          || libs_args+=(-Dlzo=disabled)
+is_listed glib       libs_deps && libs_args+=(-Dglib=enabled)         || libs_args+=(-Dglib=disabled)
+is_listed libpng     libs_deps && libs_args+=(-Dpng=enabled)          || libs_args+=(-Dpng=disabled)
+is_listed libxcb     libs_deps && libs_args+=(-Dxcb=enabled)          || libs_args+=(-Dxcb=disabled)
+is_listed freetype   libs_deps && libs_args+=(-Dfreetype=enabled)     || libs_args+=(-Dfreetype=disabled)
+is_listed fontconfig libs_deps && libs_args+=(-Dfontconfig=enabled)   || libs_args+=(-Dfontconfig=disabled)
 
 libs_build() {
+    if is_clang; then
+        CFLAGS+=" -Wno-incompatible-pointer-types"
+    fi
 
     meson.setup
 
     meson.compile
 
-    # fix missing stdc++
-    cmdlet.pkgconf meson-private/cairo.pc -lstdc++
+    if is_mingw; then
+        # fix missing CAIRO_WIN32_STATIC_BUILD
+        cmdlet.pkgconf meson-private/cairo.pc -DCAIRO_WIN32_STATIC_BUILD
+    else
+        # fix missing stdc++
+        cmdlet.pkgconf meson-private/cairo.pc -lstdc++
+    fi
 
-    # fix missing CAIRO_WIN32_STATIC_BUILD
-    is_mingw && cmdlet.pkgconf meson-private/cairo.pc -DCAIRO_WIN32_STATIC_BUILD
-
-    pkgfile libcairo -- meson.install --tags devel
+    cmdlet.pkgfile libcairo -- meson.install --tags devel
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
