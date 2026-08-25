2feeb131c911339542ee2e39b8ff8b63d22fa104
mtdcy <mtdcy.chen@gmail.com>
2026-08-26 05:50:29 +0800

    ♻️ libunistring: fix mingw build

--- libs/libunistring.s
+++ libs/libunistring.s
@@ -5,7 +5,7 @@ libs_lic='LGPLv3+|GPLv2'
 libs_ver=1.4.2
 libs_url=https://ftpmirror.gnu.org/gnu/libunistring/libunistring-$libs_ver.tar.gz
 libs_sha=e82664b170064e62331962126b259d452d53b227bb4a93ab20040d846fec01d8
-libs_dep=( libiconv )
+libs_dep=(libiconv)
 
 libs_args=(
     --disable-dependency-tracking
@@ -19,7 +19,7 @@ libs_args=(
     --enable-static
 )
 
-is_mingw && libs_args+=( --enable-threads=win32 )
+is_mingw && libs_args+=(--enable-threads=windows)
 
 libs_build() {
     configure
