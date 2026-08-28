cb9aac759c662b22064a2dc72c2b1b09958789d6
mtdcy <mtdcy.chen@gmail.com>
2026-08-28 19:29:13 +0800

    ♻️ patch: no mingw build

--- libs/patch.s
+++ libs/patch.s
@@ -1,14 +1,18 @@
 # Apply a diff file to an original
-
+#
 # shellcheck disable=SC2034
-libs_lic='GPL-3.0-or-later'
+
+# Git for Windows includes a native patch.exe
+libs_targets=(! windows)
+
+libs_lic=GPLv3+
 libs_ver=2.8
 libs_url=(
     https://mirrors.ustc.edu.cn/gnu/patch/patch-$libs_ver.tar.xz
     https://ftpmirror.gnu.org/gnu/patch/patch-$libs_ver.tar.xz
 )
 libs_sha=f87cee69eec2b4fcbf60a396b030ad6aa3415f192aa5f7ee84cad5e11f7f5ae3
-libs_dep=( )
+libs_dep=()
 
 libs_args=(
     --disable-option-checking
@@ -23,12 +27,15 @@ libs_args=(
 )
 
 libs_build() {
-    configure && make || return 1
+    libs.requires -Wno-incompatible-pointer-types
 
-    cmdlet  ./src/patch &&
+    configure
 
-    check patch --version
+    make
+
+    cmdlet.install  ./src/patch
+
+    cmdlet.check patch --version
 }
-
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
