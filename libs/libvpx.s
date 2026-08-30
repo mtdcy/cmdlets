c8dad9f4150a4fca21029b52773a4967bddee789
mtdcy <mtdcy.chen@gmail.com>
2026-08-30 19:57:41 +0800

    ♻️ fix libvpx build
    libvpx ./configure --help shows no --prefix, add it manually.

--- libs/libvpx.s
+++ libs/libvpx.s
@@ -1,16 +1,15 @@
-#
-#
-# shellcheck disable=SC2034
+# VP8/VP9 video codec
 
+# shellcheck disable=SC2034
 libs_lic="BSD-3-Clause"
 libs_ver=1.17.0
-libs_rev=1
+libs_rev=2
 libs_url=https://github.com/webmproject/libvpx/archive/v$libs_ver.tar.gz
-libs_rev=1
 libs_sha=1020f184046187baa2985dbde38e0691f49c44088bca7a1842b0236c6081dc0a
 
 libs_args=(
-    --disable-dependency-tracking
+    # ./configure --help shows no --prefix
+    --prefix="$PREFIX"
 
     --enable-vp8
     --enable-vp9
@@ -29,19 +28,14 @@ libs_args=(
 )
     #--disable-libyuv
 
-is_mingw && libs_args+=( --target=x86_64-win64-gcc )
+is_mingw && libs_args+=(--target=x86_64-win64-gcc)
 
 libs_build() {
-    configure 
+    configure
 
-    make 
+    make
 
     pkgfile libvpx -- make.install
 }
-
-#if [[ "$OSTYPE" == "darwin"* ]]; then
-#    sed -i 's/-Wl,--no-undefined/-Wl,-undefined,error/g' build/$MAKE/Makefile
-#    sed -i 's/-Wl,--no-undefined/-Wl,-undefined,error/g' Makefile
-#fi
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
