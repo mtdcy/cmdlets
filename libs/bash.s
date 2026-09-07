8aee0256b33e9412aa65bf52631b7c3a1f7f171c
mtdcy <mtdcy.chen@gmail.com>
2026-09-08 06:50:47 +0800

    ♻️ bash: code clean

--- libs/bash.s
+++ libs/bash.s
@@ -1,26 +1,36 @@
 # Bourne-Again SHell, a UNIX command interpreter
-#
-# HEAD version for feature inspection:
-#   #1. DON'T use this version as default interpreter
 
 # shellcheck disable=SC2034
+libs_stable=1
+
 libs_lic=GPLv3+
-libs_ver=5.3.15
-libs_url=(
-    https://mirrors.ustc.edu.cn/gnu/bash/bash-${libs_ver%.*}.tar.gz
-    https://github.com/bminor/bash/archive/refs/tags/bash-${libs_ver%.*}.tar.gz
-    https://ftpmirror.gnu.org/gnu/bash/bash-${libs_ver%.*}.tar.gz
-)
+libs_ver=5.3
+libs_rev=3
+libs_url=${_LIBS_MIRROR_GNU:-https://ftpmirror.gnu.org/gnu}/bash/bash-5.3.tar.gz
 libs_sha=6c377fd89688d0ce9bef112ce82c83418f1b6d5457ad6ea2ef2d8558bd552f2c
 
-PATCHLEVEL=${libs_ver##*.}
+# debian/ubuntu patches
+libs_resources=(
+    https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/bash/5.3-3ubuntu1/bash_5.3-3ubuntu1.debian.tar.xz
+)
 
-libs_resources=()
-for ((i = 1; i <= PATCHLEVEL; i++)); do
-    libs_resources+=("https://mirrors.ustc.edu.cn/gnu/bash/bash-5.3-patches/bash53-$( printf "%03d" "$i")")
-done
+# @debian/patches/series
+libs_patches=(
+    debian/patches/bash53-001.diff
+    debian/patches/bash53-002.diff
+    debian/patches/bash53-003.diff
+    debian/patches/bash53-004.diff
+    debian/patches/bash53-005.diff
+    debian/patches/bash53-006.diff
+    debian/patches/bash53-007.diff
+    debian/patches/bash53-008.diff
+    debian/patches/bash53-009.diff
+    debian/patches/input-err.diff
+    debian/patches/bash-aliases-repeat.diff
+    debian/patches/readline-need-extern-pc.diff
+)
 
-libs_deps=(ncurses readline libiconv)
+libs_deps=(ncurses readline)
 libs_args=(
     # ncurses + readline
     --with-curses
@@ -30,6 +40,7 @@ libs_args=(
     # no nls nor libintl
     --disable-nls
     --without-libintl-prefix
+    --without-libiconv-prefix
 
     # https://github.com/robxu9/bash-static/blob/master/build.sh
     --without-bash-malloc
@@ -41,47 +52,34 @@ is_darwin || libs_args+=(--enable-static-link)
 is_darwin || libs_args+=(--build="$( uname -m)-unknown-linux-gnu")
 
 if is_cygwin; then
-    # https://github.com/msys2/MSYS2-packages/tree/master/bash
-    libs_patches=(
+    libs_patches+=(
+        #https://cygwin.com/cgit/cygwin-packages/bash/plain/bash-5.2-cygwin.patch
         https://github.com/msys2/MSYS2-packages/raw/refs/heads/master/bash/0001-bash-4.4-cygwin.patch
-        https://github.com/msys2/MSYS2-packages/raw/refs/heads/master/bash/0002-bash-4.3-msysize.patch
-        https://github.com/msys2/MSYS2-packages/raw/refs/heads/master/bash/0005-bash-4.3-msys2-fix-lineendings.patch
-        https://github.com/msys2/MSYS2-packages/raw/refs/heads/master/bash/0006-bash-4.3-add-pwd-W-option.patch
         https://github.com/msys2/MSYS2-packages/raw/refs/heads/master/bash/0007-fix-static-build.patch
     )
-    #libs_args+=(
-    #    bash_cv_dev_stdin=present
-    #    bash_cv_dev_fd=standard
-    #    bash_cv_termcap_lib=libncurses
-    #)
+    libs_args+=(
+        bash_cv_dev_stdin=present
+        bash_cv_dev_fd=standard
+        #bash_cv_termcap_lib=libncurses
+    )
 fi
 
 libs_build() {
-    for x in bash53-*; do
-        slogi "$_EMOJI_RUN" "patch $x"
-        patch -Nbp0 < "$x" || patch -Nbp1 < "$x"
-    done
+    # 回收版本号
+    cmdlet.disclaim 5.3.15
 
     # macOS defined this:
     #  refer to https://github.com/Homebrew/homebrew-core/blob/90c02007778049214b6c76120bb74ef702eec449/Formula/b/bash.rb
-    CFLAGS+=" -DSSH_SOURCE_BASHRC"
+    libs.requires -DSSH_SOURCE_BASHRC
 
-    # some version needs this
-    CPPFLAGS="$CFLAGS"
-
-    # musl has strtoimax
-    if is_musl; then
-        # https://github.com/robxu9/bash-static/blob/master/custom/bash-musl-strtoimax-debian-1023053.patch
-        sed -i 's/bash_cv_func_strtoimax =.*;/bash_cv_func_strtoimax = no;/' m4/strtoimax.m4
-        slogcmd autoconf -f
-    elif is_cygwin; then
-        slogcmd autoconf -f
-        libs.requires -D__MSYS__ # patch: 0005-bash-4.3-msys2-fix-lineendings.patch
-    fi
+    slogcmd autoconf -f
 
     configure
 
-    make
+    make bash$_BINEXT
+
+    # find out real version
+    libs_ver+=".$(grep -Fw "#define PATCHLEVEL" patchlevel.h | cut -d' ' -f3)"
 
     # install
     cmdlet.install bash
