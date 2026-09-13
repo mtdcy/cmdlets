8fc2840a4ff476f72111f63629bf28072d9e2af8
mtdcy <mtdcy.chen@gmail.com>
2026-09-14 05:53:09 +0800

    ♻️ no krb5 for cygwin

--- libs/krb5.s
+++ libs/krb5.s
@@ -1,12 +1,15 @@
 # Network authentication protocol
 
 # shellcheck disable=SC2034
+libs_targets=(linux darwin)
+
 libs_lic=''
 libs_ver=1.22.2
 libs_rev=1
 libs_url=https://kerberos.org/dist/krb5/1.22/krb5-1.22.2.tar.gz
 libs_sha=3243ffbc8ea4d4ac22ddc7dd2a1dc54c57874c40648b60ff97009763554eaf13
-libs_dep=(openssl libedit)
+
+libs_deps=(openssl libedit)
 
 libs_args=(
     --disable-dependency-tracking
