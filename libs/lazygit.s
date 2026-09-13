807a8418aedbbf1d4e9696b88c6769e9b24c4058
mtdcy <mtdcy.chen@gmail.com>
2026-09-14 05:03:45 +0800

    ♻️ fix lazygit build

--- libs/lazygit.s
+++ libs/lazygit.s
@@ -20,7 +20,8 @@ libs_build() {
     go build .
 
     cmdlet.install lazygit
-    cmdlet.verify -- lazygit
+
+    cmdlet.verify -- lazygit --version
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
