a76941112f641a566b21bbf54a110e3096b41097
mtdcy <mtdcy.chen@gmail.com>
2026-09-16 22:31:09 +0800

    ♻️ libwebp: fix cygwin build

--- libs/libwebp.s
+++ libs/libwebp.s
@@ -9,10 +9,20 @@ libs_sha=93a852c2b3efafee3723efd4636de855b46f9fe1efddd607e1f42f60fc8f2136
 libs_dep=(libpng giflib libtiff libjpeg-turbo)
 
 libs_args=(
-    -DWEBP_BUILD_CWEBP=ON
-    -DWEBP_BUILD_DWEBP=ON
-
     -DBUILD_SHARED_LIBS=OFF
+)
+
+# cygwin-gcc failed to build these utils
+is_cygwin && libs_args+=(
+    -DWEBP_BUILD_ANIM_UTILS=OFF
+    -DWEBP_BUILD_CWEBP=OFF
+    -DWEBP_BUILD_DWEBP=OFF
+    -DWEBP_BUILD_GIF2WEBP=OFF
+    -DWEBP_BUILD_IMG2WEBP=OFF
+    -DWEBP_BUILD_VWEBP=OFF
+    -DWEBP_BUILD_WEBPINFO=OFF
+    -DWEBP_BUILD_WEBPMUX=OFF
+    -DWEBP_BUILD_EXTRAS=OFF
 )
 
 libs_build() {
@@ -20,16 +30,26 @@ libs_build() {
 
     cmake.build
 
-    cmdlet.pkgfile libwebp -- cmake.install
+    # cmake.install will always install utils
+    cmake.install
+    sed install_manifest.txt \
+        -e "\#$PREFIX/bin#d" \
+        -e "\#$PREFIX/share/man#d" > libwebp.txt
 
-    cmdlet.install ./build/cwebp
-    cmdlet.install ./build/dwebp
-    cmdlet.install ./build/img2webp
-    cmdlet.install ./build/webpinfo
-    cmdlet.install ./build/webpmux
-    cmdlet.install ./build/webp_quality
+    cmdlet.pkgfile libwebp < libwebp.txt
 
-    cmdlet.verify -- img2webp -version
+    if ! is_cygwin; then
+        local utils=(
+            webpinfo
+            cwebp dwebp webpmux
+            img2webp gif2webp
+        )
+        for x in "${utils[@]}"; do
+            cmdlet.install "$x"
+        done
+
+        cmdlet.verify -- webpinfo -version
+    fi
 }
 
 # vim:ft=sh:syntax=bash:ff=unix:fenc=utf-8:et:ts=4:sw=4:sts=4
