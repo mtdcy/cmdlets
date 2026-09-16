cc99c6925a8359317e654bfaff5869d0fd786cf6
mtdcy <mtdcy.chen@gmail.com>
2026-09-16 22:36:54 +0800

    ♻️ libxcrypt: fix cygwin build

--- libs/libxcrypt.s
+++ libs/libxcrypt.s
@@ -1,28 +1,24 @@
 # Extended crypt library for descrypt, md5crypt, bcrypt, and others
 #
 # shellcheck disable=SC2034
-libs_lic="LGPL-2.1-or-later"
+libs_lic="LGPLv2.1+"
 libs_ver=4.5.2
 libs_url=https://github.com/besser82/libxcrypt/releases/download/v$libs_ver/libxcrypt-$libs_ver.tar.xz
 libs_sha=71513a31c01a428bccd5367a32fd95f115d6dac50fb5b60c779d5c7942aec071
-libs_dep=()
+
+libs_deps=()
 
 libs_args=(
-    --disable-option-checking
-    --disable-dependency-tracking
-    --disable-silent-rules
+    # static only
+    --enable-static --disable-shared
 
     --disable-obsolete-api
     --disable-xcrypt-compat-files
     --disable-failure-tokens
     --disable-valgrind
-
-    --with-pic
-
-    # static only
-    --disable-shared
-    --enable-static
 )
+
+is_cygwin && libs_args+=(--disable-symvers)
 
 libs_build() {
     configure
