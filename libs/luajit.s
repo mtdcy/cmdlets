67a4a44bf62230b4144786bf1be2db8fadc1659c
mtdcy <mtdcy.chen@gmail.com>
2026-09-17 16:19:24 +0800

    ♻️ luajit: fix cygwin build

--- libs/luajit.s
+++ libs/luajit.s
@@ -12,21 +12,43 @@ libs_deps=()
 libs_args=(
     PREFIX="'$PREFIX'"
 
-    CC="'$CC'"
-    CFLAGS="'$CFLAGS'"
-    LDFLAGS="'$LDFLAGS'"
+    # HOST
+    HOST_CC="$HOSTCC"
+
+    # TARGET
+    STATIC_CC="$CC"
+    TARGET_LD="$CC"
+    TARGET_AR="$AR rcus"
+    TARGET_STRIP="$STRIP"
+
+    TARGET_CFLAGS="$CFLAGS $CPPFLAGS"
+    TARGET_LDFLAGS="$LDFLAGS"
 
     BUILDMODE=static
 )
+
+# buildvm -m peobj
+is_cygwin && libs_args+=(TARGET_SYS=Windows)
 
 libs_build() {
     cmdlet.disclaim 2.1
 
     make "${libs_args[@]}"
 
-    cmdlet.pkgfile libluajit -- make install "${libs_args[@]}"
+    # ABIVER : 5.1
+    cp -f src/libluajit.a src/libluajit-5.1.a
 
-    # install as versioned and link to luajit
+    cmdlet.pkginst libluajit \
+            include/luajit-2.1  src/luajit.h \
+                                src/luaconf.h \
+                                src/lualib.h \
+                                src/lua.hpp \
+                                src/lauxlib.h \
+                                src/lua.h \
+            lib                 src/libluajit-5.1.a \
+            lib/pkgconfig       etc/luajit.pc
+
+    # install luajit as versioned and link to luajit
     cmdlet.install ./src/luajit luajit-$libs_ver luajit
 
     cmdlet.verify -- luajit -v
